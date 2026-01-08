8eda42a45183d8644bc577c46fe594ae02e531f1
Muhammad Irwan Andriawan <irwan@andriawan.com>
2026-01-08 14:16:13 +0700

    feat: add HTTPS enforcement and QR callback route

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::post('/qr-callback', function () {
+    $request = Request::capture()->all();
+    \Log::info('QR Callback Data:', $request);
+});
