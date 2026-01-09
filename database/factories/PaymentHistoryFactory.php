4e6bd9ec1fdcf2be149e31c841d76e0b89f3215c
Muhammad Irwan Andriawan <irwan@andriawan.com>
2026-01-09 22:07:30 +0700

    chore: update factory payment history

--- database/factories/PaymentHistoryFactory.php
+++ database/factories/PaymentHistoryFactory.php
@@ -21,7 +21,7 @@ class PaymentHistoryFactory extends Factory
             'trx_id' => $this->faker->unique()->uuid(),
             'qr_code' => '00020101021226760024ID.CO.SPEEDCASH.MERCHANT01189360081530002033470215ID10250020334760303UKE51440014ID.CO.QRIS.WWW0215ID10254269502140303UKE520448165303360540410005802ID5912IMAM GHOZALI6006JEMBER61056812162320108647336390509D261210430703A0163047CFA',
             'amount' => $this->faker->numberBetween(1000, 100000),
-            'status' => $this->faker->randomElement(['pending', 'completed', 'failed']),
+            'transaction_status' => $this->faker->randomElement(['00', '07']),
             'expired_at' => $this->faker->dateTimeBetween('now', '+1 week'),
         ];
     }
